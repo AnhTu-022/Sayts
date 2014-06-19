831e2844da37a2311c397a2c6434bbe9c402dad5
Mathias Gartner <mathias.gartner@aon.at>
2014-06-19 13:59:31 +0200

    Muted video tag to prevent positive feedback loop

--- templates/start.php
+++ templates/start.php
@@ -109,6 +109,7 @@
 		      navigator.getMedia({video: true, audio: true}, function(localMediaStream) {
 		          var video = document.querySelector('video');
 		          video.src = window.URL.createObjectURL(localMediaStream);
+		          video.volume = 0;
 
 		          // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
 		          // See crbug.com/110938.
